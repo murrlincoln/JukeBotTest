883a7929f3ebd774a56ae764e48c7526d900a46a
murrlincoln <65836530+murrlincoln@users.noreply.github.com>
2020-11-30 22:54:20 -0500

    First test at getting FAQ running, need to keep code in main for now

---
+++ Front End/src/components/faq/faq.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './faq.css';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+class faq extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // callback function passing state values up to Game to actually start the game
+  startGame = () => {
+    //this.state.startGame(this.state.twitterHandleOne,this.state.twitterHandleTwo)
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render(){
+    const session = "/session/"+this.state.value;
+    return (
+      <div>
+        <h1>Join Lobby</h1>
+        <input value={this.state.value} className="sessionJoin" onChange={this.handleChange} />
+        
+        <div>
+          <NavLink to={session} className="navButton">Join Session</NavLink>
+        </div>
+        
+      </div>
+    );
+  }
+  
+}
+
+export default faq;
